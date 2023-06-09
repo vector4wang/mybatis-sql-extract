d83fde78564b9d13a0bf377e658d30c3ecd13f79
GitHub Action <action@github.com>
2023-06-09 15:31:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/vector4wang/mybatissqlextract/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/vector4wang/mybatissqlextract/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vector4wang.mybatissqlextract.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
